9ad05fdfd3959f303367a90d74f6ce878e13466f
James MA <Super169@126.com>
2016-03-14 10:48:29 +0800

    Obtaining the device list
    https://github.com/PcapDotNet/Pcap.Net/wiki/Pcap.Net-Tutorial-Obtaining-the-device-list
    
    Signed-off-by: James MA <Super169@126.com>

---
+++ myPcapEx01/Form1.Designer.vb
@@ -0,0 +1,66 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGo = New System.Windows.Forms.Button()
+        Me.lvData = New System.Windows.Forms.ListView()
+        Me.SuspendLayout()
+        '
+        'btnGo
+        '
+        Me.btnGo.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnGo.Location = New System.Drawing.Point(0, 336)
+        Me.btnGo.Name = "btnGo"
+        Me.btnGo.Size = New System.Drawing.Size(834, 23)
+        Me.btnGo.TabIndex = 1
+        Me.btnGo.Text = "Retreive Device Information"
+        Me.btnGo.UseVisualStyleBackColor = True
+        '
+        'lvData
+        '
+        Me.lvData.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lvData.Location = New System.Drawing.Point(0, -1)
+        Me.lvData.Name = "lvData"
+        Me.lvData.Size = New System.Drawing.Size(834, 334)
+        Me.lvData.TabIndex = 2
+        Me.lvData.UseCompatibleStateImageBehavior = False
+        Me.lvData.View = System.Windows.Forms.View.Details
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(834, 362)
+        Me.Controls.Add(Me.lvData)
+        Me.Controls.Add(Me.btnGo)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnGo As Button
+    Friend WithEvents lvData As ListView
+End Class
---
+++ myPcapEx01/Form1.vb
@@ -0,0 +1,33 @@
+﻿Imports PcapDotNet.Core
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lvData.Columns.Add("#", 30, HorizontalAlignment.Right)
+        lvData.Columns.Add("Name", 400, HorizontalAlignment.Left)
+        lvData.Columns.Add("Description", 400, HorizontalAlignment.Left)
+    End Sub
+
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+        Dim allDevices As IList(Of LivePacketDevice)
+        Dim i As Integer
+
+        allDevices = LivePacketDevice.AllLocalMachine
+        If (allDevices.Count = 0) Then
+            MsgBox("No interfaces found!")
+            Return
+        End If
+
+        For i = 0 To allDevices.Count - 1
+            Dim device As LivePacketDevice
+            Dim str(3) As String
+            Dim itm As ListViewItem
+            device = allDevices.Item(i)
+            str(0) = (i + 1).ToString
+            str(1) = device.Name
+            str(2) = device.Description
+            itm = New ListViewItem(str)
+            lvData.Items.Add(itm)
+        Next
+    End Sub
+
+End Class
